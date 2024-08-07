ff695c13db7c95edd9fa0121fca4aa5251c0f50f
CadenceRiddle <cadence475@gmail.com>
2024-08-07 16:37:12 -0400

    used sequelize to connect to database and sync with user model

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const sequelize = require('sequelize');
-
-app.get('/', (req, res)=>{
-  res.send("hello world");
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('photo-caption-contest', 'postgres', 'Ga11ego$0908', {
+  dialect: 'postgres'
 });
 
+sequelize.authenticate().then(() => {
+  console.log('Connected successfully to database')
+}).catch((err) =>{
+  console.log('did  not connect')
+})
+
+const User = sequelize.define('user', {
+  username: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.DataTypes.STRING
+  },
+  age: {
+    type: Sequelize.DataTypes.INTEGER,
+    defaultValue: 21
+  }
+});
+
+User.sync()
+  .then(() => {
+    console.log('Table and model synced successfully');
+  })
+  .catch((err) => {
+    console.error('Failed to sync the model:', err);
+  });
 
 app.listen(PORT, () =>{
   console.log(`listening on port ${PORT}`);
