8795db19adca9e411e7370e624947a9798b11b13
CadenceRiddle <cadence475@gmail.com>
2024-08-08 15:18:11 -0400

    added get method for all users

--- app.js
+++ app.js
@@ -4,26 +4,34 @@ const PORT = 3000;
 const { User, Photo } = require('./sequelize');
 app.use(express.json());
 
-//Create photo
-app.post('/photo', async(req, res, next) => {
+// Create photo
+app.post('/photo', async (req, res, next) => {
   try {
     const newPhoto = await Photo.create(req.body);
     res.status(201).json(newPhoto);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
-  catch (err) {
-    res.status(400).json({error: err.message});
-  };
 });
 
-// create user
-app.post('/user', async(req, res, next) => {
+// Create user
+app.post('/user', async (req, res, next) => {
   try {
     const newUser = await User.create(req.body);
     res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
-  catch (err) {
-    res.status(400).json({error: err.message});
-  };
+});
+
+// Show all users
+app.get('/users', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 app.listen(PORT, () => {
