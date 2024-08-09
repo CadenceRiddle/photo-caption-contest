fbcba908029a24974b73f1ad6af9b3ba1592def8
CadenceRiddle <cadence475@gmail.com>
2024-08-08 22:49:54 -0400

    added path to encryption file

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const PORT = 3000;
 // Import the separated routes
 const photoRoutes = require('./photo_routes');
 const userRoutes = require('./user_routes');
+const encryption = require('./encryption');
 
 app.use(express.json());
 
 // Use the separated routes
 app.use('/api', photoRoutes);
 app.use('/api', userRoutes);
+app.use('/api', encryption);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
