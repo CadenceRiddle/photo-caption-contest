4b9dfd85159307d45e43cbd43c2b0e9a545f5477
CadenceRiddle <cadence475@gmail.com>
2024-08-09 16:26:25 -0400

    removed redundant create user route

--- user_routes.js
+++ user_routes.js
@@ -4,16 +4,6 @@ const { User } = require('./sequelize');
 
 router.use(express.json());
 
-// Create user
-router.post('/user', async (req, res, next) => {
-  try {
-    const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
 // Show all users
 router.get('/users', async (req, res, next) => {
   try {
